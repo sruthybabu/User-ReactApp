24b0afc7cd5bbc7fa69a88084a53deca91a3040e
sruthybabu <sruthybabu64@gmail.com>
2022-05-18 11:18:51 +0530

    completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div>
 
-<nav className="navbar navbar-expand-lg bg-light">
+<nav className="navbar navbar-expand-lg navbar-dark bg-primary">
   <div className="container-fluid">
       <Link to="/" className="navbar-brand">MZC</Link>
     
